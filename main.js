2f6c907a70c4d6d2fbe9960c73b2cea3c8ce449e
brigide <brigidegd@gmail.com>
2022-06-28 22:47:23 -0300

    created road

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
 
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9)
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
@@ -12,7 +13,13 @@ function animate() {
 
     canvas.height = window.innerHeight; //resizes vertically every frame
 
+    ctx.save();
+    ctx.translate(0, -car.y + canvas.height * 0.8);
+
+    road.draw(ctx);
     car.draw(ctx); // draws the car
+
+    ctx.restore();
 
     requestAnimationFrame(animate); // makes animation
 }
