a27b3826ee59d4c02171d1b6a098072531de3b89
brigide <brigidegd@gmail.com>
2022-06-28 22:14:19 -0300

    implemented car mechanics

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const canvas = document.getElementById('canvas');
 canvas.width = 200;
-canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
 
 const car = new Car(100, 100, 30, 50);
 
-car.draw(ctx);
+animate();
+
+function animate() {
+    car.update(); // update car every frame
+
+    canvas.height = window.innerHeight; //resizes vertically every frame
+
+    car.draw(ctx); // draws the car
+
+    requestAnimationFrame(animate); // makes animation
+}
